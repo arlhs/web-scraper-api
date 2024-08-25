4896ac6588e433c1538418fac1491c276281ab0f
Aditya Raj <rajaditya0703@gmail.com>
2024-08-26 04:31:48 +0530

    updated schema

--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, Field
 
 class UrlPages(BaseModel):
     url: str
-    pages: int
+    pages: int = Field(default=1, gt=0)
+
+    @field_validator('pages')
+    def check_price(cls, value):
+        if value < 1:
+            raise ValueError('Page must be greater than 0')
+        return value
